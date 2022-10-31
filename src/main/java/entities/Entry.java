d47592e0a817d62fa09035ca0396b51cc8ae4c34
Parth <parthsingh2324@gmail.com>
2022-10-31 14:29:03 -0400

    copy commit

--- src/main/java/entities/Entry.java
+++ src/main/java/entities/Entry.java
@@ -1,8 +1,34 @@
 package main.java.entities;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.time.Instant;
+import java.util.Serializable;
+
 /**
  * Represents an entry of a journal.
  */
-public abstract class Entry{
-    
+public abstract class Entry implements Serializable{
+    public enum ENTRY_TYPE{
+        RECIPE_RECORD,
+        SEARCH_RECORD
+    }
+
+    private final static Comparator<Entry> chrono_compare = new Comparator<Entry>(){
+        public int compare(Entry one, Entry two){
+            return two.time.compareTo(one.time);
+        }
+    };
+
+    private Instant time;
+    private ENTRY_TYPE type;
+
+    protected Entry(Instant time, ENTRY_TYPE type){
+        this.time = time;
+        this.type = type;
+    }
+
+    public static void sortChronologically(Entry[] entries){
+        Arrays.sort(entries, chrono_compare);
+    }
 }
