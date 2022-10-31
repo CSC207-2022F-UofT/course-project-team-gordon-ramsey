f1d94eefdb11501f3061cdab777a23bbb477bcf5
Parth <parthsingh2324@gmail.com>
2022-10-31 08:30:38 -0400

    added basic attributes for User

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,8 +1,13 @@
 package main.java.entities;
 
+import java.io.Serializable;
+
 /**
  * Represents a user profile.
  */
-public class User{
+public class User implements Serializable{
+    private String fullname;
+    private String name;
+    private GroceryList glist;
     private Journal journal;
 }
