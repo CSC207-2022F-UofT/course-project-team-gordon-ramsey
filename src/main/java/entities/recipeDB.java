1c14fa6e3c630039977559a9228d0c2fef45f6d3
Alex Figner <alexfigner@gmail.com>
2022-11-14 15:25:35 -0500

    Fixed return statement in recipeDB

--- src/main/java/entities/recipeDB.java
+++ src/main/java/entities/recipeDB.java
@@ -4,10 +4,10 @@ public class recipeDB extends DB {
 
     public recipeDB (){}
 
+    /*
     public boolean addRecipe(Recipe r){
-        /* Needs implementation. Should add take in Recipe and return true if successfully added to DB*/
-        /*/ if successful
-         */
+        Needs implementation. Should add take in Recipe and return true if successfully added to DB
         return true;
     }
+    */
 }
