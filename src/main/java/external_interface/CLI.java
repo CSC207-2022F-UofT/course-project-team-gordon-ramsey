b446f3fee59b06225dda438d3571267fd9cfd4ad
JisuHong <jaylovegood@snu.ac.kr>
2022-11-20 13:42:19 -0500

    basic structure for the login/signup cycle

--- src/main/java/external_interface/CLI.java
+++ src/main/java/external_interface/CLI.java
@@ -1,5 +1,26 @@
 package external_interface;
 
+import java.util.Scanner;
+
 public class CLI {
 
+    public void master(){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            boolean result = identifyUser();
+        }
+
+    }
+    public boolean identifyUser(){
+        System.out.println("Welcome!");
+        return false;
+    }
+
+    public boolean login(){
+        return false;
+    }
+
+    public boolean signup(){
+        return false;
+    }
 }
