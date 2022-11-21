7ec364b3442a5ad1d1a64cbd88745293026e5f99
JisuHong <jaylovegood@snu.ac.kr>
2022-11-20 22:54:36 -0500

    defined login, signup and helper functions

--- src/main/java/external/interfaces/CLI.java
+++ src/main/java/external/interfaces/CLI.java
@@ -12,9 +12,92 @@ public class CLI implements UI{
     private Presenter presenter;
     private Scanner reader;
 
+    private final String[] IDENTIFICATION_COMMANDS = {"login", "sign up"};
+
+    // Allows the user to choose from a given list of commands.
+    // returns null if the input is invalid
+    private String chooseMenu(String[] menus){
+        printMenu(menus);
+        String userInput = reader.nextLine();
+        return validateChoice(menus, userInput);
+    }
+
+    private void printMenu(String[] menus){
+        showMessage("MENU");
+        for (String menu : menus){
+            System.out.print(menu + "\t");
+        }
+    }
+
+    private String validateChoice(String[] menus, String userInput){
+        boolean found = false;
+        for (String menu : menus){
+            if (menu.equals(userInput)){
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            return userInput;
+        }
+        else {
+            return null;
+        }
+    }
+
     public CLI(Presenter presenter){
         this.presenter = presenter;
         this.reader = new Scanner(System.in);
+    }
+
+    public void run(){
+        System.out.println("Welcome to the recipe app!");
+        identifyUser(); // FIXME
+    }
+
+
+    private boolean identifyUser(){
+
+        // FIXME how can the UI know if the login/signup was successful?
+        // (how can it break from the loop?)
+        // should this be the presenter's job?
+        showMessage("Please login or sign up to continue.");
+        String command = chooseMenu(IDENTIFICATION_COMMANDS);
+        switch (command) {
+            case "login" :
+                login();
+                return true;
+            case "sign up":
+                signup();
+                return true;
+            default:
+                invalidCommand();
+                return false;
+        }
+    }
+
+    public void login(){
+        showMessage("enter your username: ");
+        String username = reader.nextLine();
+        showMessage("enter your password: ");
+        String password = reader.nextLine();
+        String[] data = {username, password};
+        ChangeEvent e = new ChangeEvent(USE_CASE.USER_LOGIN_USECASE, data);
+        presenter.fireEvent(e);
+    }
+
+    public void signup(){
+        showMessage("enter your username: ");
+        String username = reader.nextLine();
+        showMessage("enter your password: ");
+        String password = reader.nextLine();
+        String[] data = {username, password};
+        ChangeEvent e = new ChangeEvent(USE_CASE.CREATE_USER_USECASE, data);
+        presenter.fireEvent(e);
+    }
+
+    public void invalidCommand(){
+        showMessage("Error: Invalid command");
     }
 
     public String getInput(){
