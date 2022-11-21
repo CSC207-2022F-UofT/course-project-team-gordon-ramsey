804c062d3b162d3f99c9a794d4ba0619f550d300
kumkum <choudhary.kumkum@mail.utoronto.ca>
2022-11-21 01:03:55 -0500

    Updated CLI

--- src/main/java/business/rules/usecases/AddNewRecipeUseCase.java
+++ src/main/java/business/rules/usecases/AddNewRecipeUseCase.java
@@ -1,5 +1,7 @@
-package business.rules;
+package business.rules.usecases;
 
+import business.rules.UseCaseRequest;
+import business.rules.UseCaseResponse;
 import entities.Ingredient;
 import entities.Instruction;
 import entities.Recipe;
@@ -14,5 +16,18 @@ public class AddNewRecipeUseCase {
 
     public Recipe newRecipe(String name, String description, Ingredient[] ingredients, Instruction[] instructions, Duration cook_time){
         return new Recipe(name, description, ingredients, instructions, cook_time);
+    }
+
+    public UseCaseResponse process(UseCaseRequest ucr){
+        // initiate work here
+        return null;
+    }
+
+    public int getEndStage(){
+        return 1;
+    }
+
+    public String getJob(){
+        return "adding new recipe";
     }
 }
