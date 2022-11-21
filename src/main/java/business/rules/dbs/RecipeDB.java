fefd0ec6a8453c686d83680f0f6a0ae0002de15b
Ansh2905 <anshdpatel10@gmail.com>
2022-11-20 19:07:13 -0500

    Completed the SearchRecipeUseCase

--- src/main/java/business/rules/dbs/RecipeDB.java
+++ src/main/java/business/rules/dbs/RecipeDB.java
@@ -1,4 +1,9 @@
 package business.rules.dbs;
 
 public class RecipeDB extends DB{
+    public boolean validateSearchTerm(String searchTerm){
+        //if searchTerm is a keyword in the recipe database, return true
+        return true;
+        //else return false
+    }
 }
--- src/main/java/business/rules/usecases/SearchRecipeUsecase.java
+++ src/main/java/business/rules/usecases/SearchRecipeUsecase.java
@@ -3,12 +3,27 @@ package business.rules.usecases;
 import business.rules.UseCase;
 import business.rules.UseCaseRequest;
 import business.rules.UseCaseResponse;
+import business.rules.dbs.RecipeDB;
+
 
 public class SearchRecipeUsecase implements UseCase{
-    
+    private final String[] searchSuccess = {"Search was a success!"};
+    private final String[] searchFailure = {"Search failed!"};
+    @Override
     public UseCaseResponse process(UseCaseRequest ucr){
-        // initiate work here
-        return null;
+        String keyword = (String) ucr.data[0];
+        RecipeDB recipeDB = (RecipeDB) ucr.data[1];
+        boolean recipeResponse = recipeDB.validateSearchTerm(keyword);
+        if(recipeResponse)
+        {
+            return new UseCaseResponse(UseCaseResponse.RETURN_CODE.SUCCESS,
+                    UseCaseResponse.ACTION_CODE.SHOW_DATA_STRING, searchSuccess);
+        }
+        else
+        {
+            return new UseCaseResponse(UseCaseResponse.RETURN_CODE.FAILURE,
+                    UseCaseResponse.ACTION_CODE.SHOW_DATA_STRING, searchFailure);
+        }
     }
 
     public int getEndStage(){
