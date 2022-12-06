fbe19c9cc1ec94bd044f8e1c3481bdc5d60c7c10
Parth <parthsingh2324@gmail.com>
2022-12-06 00:50:56 -0500

    moved db work to main

--- src/main/java/business/rules/dbs/SerializableReader.java
+++
@@ -1,5 +0,0 @@
-package business.rules.dbs;
-
-public abstract class SerializableReader<T> extends SerializableIOHandler<T>{
-    public abstract T[] read();
-}
--- src/main/java/business/rules/dbs/SerializableWriter.java
+++
@@ -1,5 +0,0 @@
-package business.rules.dbs;
-
-public abstract class SerializableWriter<T> extends SerializableIOHandler<T>{
-    public abstract void write(T obj);
-}
