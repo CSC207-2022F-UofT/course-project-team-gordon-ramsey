a2c93d482149cc652f8ec15a9663815560c79011
Ansh2905 <anshdpatel10@gmail.com>
2022-12-08 15:36:32 -0500

    Wrote JavaDoc for IngredientTest

--- src/Test/entities_testing/IngredientTest.java
+++ src/Test/entities_testing/IngredientTest.java
@@ -3,16 +3,28 @@ import entities.Ingredient;
 
 import junit.framework.TestCase;
 
+/**
+ * Contains tests for the methods of the Ingredient entity, which can be found in the entities subfolder of the main
+ * folder
+ */
 public class IngredientTest extends TestCase {
-
+    /**
+     * Setup phase which runs before every test method
+     */
     public void setUp() throws Exception {
         super.setUp();
     }
 
+    /**
+     * Teardown phase which runs after every test method
+     */
     public void tearDown() throws Exception {
         super.tearDown();
     }
 
+    /**
+     * Tests to check if the getName() method works when all possible parameters are provided for the Ingredients entity
+     */
     public void testGetNameGivenAllParameters(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", "agua", quantity);
@@ -20,6 +32,10 @@ public class IngredientTest extends TestCase {
         assertEquals(ing.getName(), "water");
     }
 
+    /**
+     * Tests to check if the getName() method works when the "description" parameter is not provided when creating
+     * an instance of the Ingredient entity
+     */
     public void testGetNameNotGivenDescription(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", quantity);
@@ -27,6 +43,9 @@ public class IngredientTest extends TestCase {
         assertEquals(ing.getName(), "water");
     }
 
+    /**
+     * Tests to check if the getDescription() method works when all parameters are provided for the Ingredient entity
+     */
     public void testGetDescriptionGivenAllParameters(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", "agua", quantity);
@@ -34,6 +53,10 @@ public class IngredientTest extends TestCase {
         assertEquals(ing.getDescription(), "agua");
     }
 
+    /**
+     * Tests to check if the getDescription() method works when the "description" parameter is not provided when
+     * creating an instance of the Ingredient entity
+     */
     public void testGetDescriptionNotGivenDescription(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", quantity);
@@ -41,6 +64,9 @@ public class IngredientTest extends TestCase {
         assertEquals(ing.getDescription(), "100 ML Water");
     }
 
+    /**
+     * Tests to check if the getQuantity() method works when all parameters are provided for the Ingredient entity
+     */
     public void testGetQuantityGivenAllParameters(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", "agua", quantity);
@@ -48,6 +74,10 @@ public class IngredientTest extends TestCase {
         assertEquals(ing.getQuantity(), quantity);
     }
 
+    /**
+     * Tests to check if the getQuantity() method works when the "description" parameter is not provided when
+     * creating an instance of the Ingredient entity
+     */
     public void testGetQuantityNotGivenDescription(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", quantity);
@@ -55,6 +85,9 @@ public class IngredientTest extends TestCase {
         assertEquals(ing.getQuantity(), quantity);
     }
 
+    /**
+     * Tests to check if the setQuantity() method works when all parameters are provided for the Ingredient entity
+     */
     public void testSetQuantityGivenAllParameters(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", "agua", quantity);
@@ -65,6 +98,10 @@ public class IngredientTest extends TestCase {
 
     }
 
+    /**
+     * Tests to check if the setQuantity() method works when the "description" parameter is not provided when
+     * creating an instance of the Ingredient entity
+     */
     public void testSetQuantityNotGivenDescription(){
         Quantity quantity = new Quantity(100, "ML");
         Ingredient ing = new Ingredient("water", quantity);
