01a0784ee374cf51927291436ed078350b8e4578
Alex Figner <alexfigner@gmail.com>
2022-12-08 22:03:41 -0500

    Added documentation to Presenter and UseCaseHandler

--- src/main/java/business/rules/Presenter.java
+++ src/main/java/business/rules/Presenter.java
@@ -16,7 +16,17 @@ public class Presenter {
     private UserDB udb;
     private User active_user;
     private Recipe selected_recipe;
-    
+
+    /**
+     * Sequentially builds the necessary elements of the presenter
+     * @param ui UI with which to set initial view
+     * @param usr SDR of UserDataPackets to read User data from local DB
+     * @param usw SDW of UserDataPackets to write User data to local DB
+     * @param rsr SDR of RecipeDataPackets to read Recipe data from local DB
+     * @param rsw SDW of RecipeDataPackets to write Recipe data to local DB
+     * @param api APIReader to pull JSON from API to DB for parsing
+     * @return Returns the built, active presenter with all necessary components
+     */
     public static Presenter buildPresenter(UI ui, SerializableDatabaseReader<UserDataPacket> usr,
                                            SerializableDatabaseWriter<UserDataPacket> usw,
                                            SerializableDatabaseReader<RecipeDataPacket> rsr,
@@ -28,6 +38,15 @@ public class Presenter {
         return p;
     }
 
+    /**
+     *
+     * @param ui UI to be used by this presenter
+     * @param usr SDR of UserDataPackets to read User data from local DB
+     * @param usw SDW of UserDataPackets to write User data to local DB
+     * @param rsr SDR of RecipeDataPackets to read Recipe data from local DB
+     * @param rsw SDW of RecipeDataPackets to write Recipe data to local DB
+     * @param api APIReader to pull JSON from API to DB for parsing
+     */
     private Presenter(UI ui, SerializableDatabaseReader<UserDataPacket> usr,
                       SerializableDatabaseWriter<UserDataPacket> usw,
                       SerializableDatabaseReader<RecipeDataPacket> rsr,
@@ -40,6 +59,14 @@ public class Presenter {
         this.selected_recipe = null;
     }
 
+    /**
+     * Initializes necessary databases
+     * @param usr SDR of UserDataPackets to initialize
+     * @param usw SDW of UserDataPackets to initialize
+     * @param rsr SDR of RecipeDataPackets to initialize
+     * @param rsw SDW of RecipeDataPackets to initialize
+     * @param api APIReader to initialize
+     */
     private void initDatabases(SerializableDatabaseReader<UserDataPacket> usr,
                                SerializableDatabaseWriter<UserDataPacket> usw,
                                SerializableDatabaseReader<RecipeDataPacket> rsr,
@@ -48,18 +75,33 @@ public class Presenter {
         this.udb = UserDB.getLocalInstance(usr, usw, this);
     }
 
+    /**
+     * Fires ChangeEvent to UseCaseHandler to be processed
+     * @param e ChangeEvent containing information needed from Presenter for UseCases
+     */
     public void fireEvent(ChangeEvent e){
         this.uch.handle(e);
     }
 
+    /**
+     * Passes UI a string to be displayed
+     * @param str String to be displayed to user
+     */
     public void showUser(String str){
         this.ui.showMessage(str);
     }
 
+    /**
+     * Passes UI a Recipe to be displayed in collection form
+     * @param r Recipe to be displayed to user in collection form
+     */
     public void showUser(Recipe r){
         this.ui.showCollection(r.getCollection());
     }
-
+    /**
+     * Passes UI an Array of Recipes to be displayed in collection form
+     * @param r Recipes to be displayed to user in collection form
+     */
     public void showUser(Recipe[] r){
         String[][][] collection = new String[r.length][][];
         for(int i = 0; i < collection.length; i++){
@@ -68,40 +110,73 @@ public class Presenter {
         this.ui.showCollection(collection);
     }
 
+    /**
+     * Passes the active RecipeDB
+     * @return The active RecipeDB
+     */
     public RecipeDB getRecipeDB(){
         return this.rdb;
     }
-
+    /**
+     * Passes the active UserDB
+     * @return The active UserDB
+     */
     public UserDB getUserDB(){
         return this.udb;
     }
-  
+
+    /**
+     * Safely closes the active DBs
+     */
     public void close(){
         this.rdb.close();
         this.udb.close();
     }
 
+    /**
+     * Sets active user
+     * @param usr User to be set as active
+     */
     public void setUser(User usr){
         this.active_user = usr;
     }
 
+    /**
+     * Passes the active User
+     * @return Returns the active User
+     */
     public User getUser(){
         return this.active_user;
     }
 
+    /**
+     * Passes the currently selected Recipe
+     * @return The selected Recipe
+     */
     public Recipe getSelectedRecipe(){
         return this.selected_recipe;
     }
 
+    /**
+     * Returns if there is an active User
+     * @return True if there is an active user, False otherwise
+     */
     public boolean hasActiveUser(){
         return !(this.active_user == null);
     }
 
+    /**
+     * Displays the selected Recipe to the UI. If no recipe is selected, displays a String notifying the user
+     *
+     */
     public void showSelectedRecipe(){
         if(this.selected_recipe == null) this.ui.showMessage("No Recipe Selected.");
         else this.ui.showCollection(this.selected_recipe.getCollection());
     }
 
+    /**
+     * Displays the active User's favourite Recipes. If no User is active, displays a String notifying the user
+     */
     public void showFavoriteRecipes(){
         if(this.active_user == null){
             this.ui.showMessage("No active user ! Unable to show favorite recipes.");
@@ -110,6 +185,9 @@ public class Presenter {
         this.ui.showCollection(this.active_user.getJournal().getFavoritesCollection());
     }
 
+    /**
+     * Displays the active User's journal. If no User is active, displays a String notifying the user
+     */
     public void showJournal(){
         if(this.active_user == null){
             this.ui.showMessage("No active user ! Unable to show journal.");
@@ -118,6 +196,9 @@ public class Presenter {
         this.ui.showCollection(this.active_user.getJournal().getCollection());
     }
 
+    /**
+     * Passes the active User's Grocery List to the UI. If no User is active, displays a String notifying the user
+     */
     public void showGroceryList(){
         if(this.active_user == null){
             this.ui.showMessage("No active user ! Unable to show grocery list.");
@@ -126,6 +207,9 @@ public class Presenter {
         this.ui.showCollection(this.active_user.getGroceryList().getCollection());
     }
 
+    /**
+     * Logs out the Active user and displays success. If no User is active, notifies user via String.
+     */
     public void logoutUser(){
         if(this.active_user == null){
             this.ui.showMessage("No active user ! Unable to logout.");
