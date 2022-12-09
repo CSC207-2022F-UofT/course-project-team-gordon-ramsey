3b4bb2165475465e34f6cd83f6f4408090ee9eba
kumkum1 <66388655+kumkum1@users.noreply.github.com>
2022-12-08 22:08:30 -0500

    added javadoc to API

--- src/main/java/business/rules/api/APIResponse.java
+++ src/main/java/business/rules/api/APIResponse.java
@@ -1,8 +1,15 @@
 package business.rules.api;
 
+/**
+ * API response string from NetReader
+ */
 public class APIResponse{
     public String[][] data;
 
+    /**
+     *
+     * @param data the String[][] response data
+     */
     public APIResponse(String[][] data){
         this.data = data;
     }
